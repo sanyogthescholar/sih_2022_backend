11cfbfa06809d43a14c2387f2fe8bb27aca020b2
sanyogthescholar <sanyogjain5678@gmail.com>
2022-03-14 00:33:32 +0530

    QR Code Generation works

--- src/controllers/todos/index.ts
+++ src/controllers/todos/index.ts
@@ -1,11 +1,17 @@
 import { Response, Request } from "express"
 import { ITicket } from "./../../types/ticket"
 import Ticket from "../../models/ticket"
+const qrcode = require('qrcode');
+const qrOption = { 
+  margin : 7,
+  width : 175
+};
 
 const getTickets = async (req: Request, res: Response): Promise<void> => {
     try {
-        const todos: ITicket[] = await Ticket.find()
-        res.status(200).json({ todos })
+        const booked_tickets: ITicket[] = await Ticket.find({"checked_in":false})
+        const checked_in: ITicket[] = await Ticket.find({"checked_in":true})
+        res.status(200).json({ booked_tickets, checked_in })
     }
     catch (error) {
         throw error
@@ -34,10 +40,32 @@ const addTickets = async (req: Request, res: Response): Promise<void> => {
 
         const newTicket: ITicket = await ticket.save()
         const allTicket: ITicket[] = await Ticket.find()
+        const qrString = req.body.hash;
+        const bufferImage = await qrcode.toDataURL(qrString,qrOption);
+        console.log(bufferImage);
+
+        // Decoding base-64 image
+        // Source: http://stackoverflow.com/questions/20267939/nodejs-write-base64-image-file
+        /*function decodeBase64Image(dataString: string) 
+        {
+          var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+          var response = {};
+
+          if (matches.length !== 3) 
+          {
+            return new Error('Invalid input string');
+          }
+
+          response.type = matches[1];
+          response.data = new Buffer(matches[2], 'base64');
+
+          return response;
+        }
+        var imageBuffer = decodeBase64Image(bufferImage);*/
 
         res
-            .status(201)
-            .json({message: "Ticket added", ticket: newTicket, tickets: allTicket})
+            .status(200)
+            .json({ticket: bufferImage})
     }
     catch (error) {
         throw error
